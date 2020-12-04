23530678c36e4429c7dfa34b7f5eb4862f211212
Sion Pixley <sionpixley@gmail.com>
2020-12-04 16:19:53 -0600

    Library appears to be working for now. Will add more safety specific features later.

--- Sion.CsvParse/Csv.cs
+++ Sion.CsvParse/Csv.cs
@@ -7,10 +7,10 @@ namespace Sion.CsvParse {
     public static class Csv {
         public static async Task<IEnumerable<IEnumerable<string>>> Parse(string path) {
             IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
-            List<List<string>> data = new List<List<string>>();
+            List<IEnumerable<string>> data = new List<IEnumerable<string>>();
 
             foreach(var line in lines) {
-                data.Add(line.Split(',').ToList());
+                data.Add(line.Split(','));
             }
 
             return data;
@@ -18,13 +18,41 @@ namespace Sion.CsvParse {
 
         public static async Task<IEnumerable<IEnumerable<string>>> Parse(string path, char delimiter) {
             IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
-            List<List<string>> data = new List<List<string>>();
+            List<IEnumerable<string>> data = new List<IEnumerable<string>>();
 
             foreach(var line in lines) {
-                data.Add(line.Split(delimiter).ToList());
+                data.Add(line.Split(delimiter));
             }
 
             return data;
+        }
+
+        public static async Task<IEnumerable<string>> GetHeaders(string path) {
+            IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
+            IEnumerable<string> headers = lines.ElementAt(0).Split(',');
+            return headers;
+        }
+
+        public static async Task<IEnumerable<string>> GetHeaders(string path, char delimiter) {
+            IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
+            IEnumerable<string> headers = lines.ElementAt(0).Split(delimiter);
+            return headers;
+        }
+
+        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path) {
+            return await Parse(path);
+        }
+
+        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path, char delimiter) {
+            return await Parse(path, delimiter);
+        }
+
+        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path, bool hasHeaders) {
+            return ((hasHeaders) ? (await Parse(path)).Skip(1) : await Parse(path));
+        }
+
+        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path, char delimiter, bool hasHeaders) {
+            return ((hasHeaders) ? (await Parse(path, delimiter)).Skip(1) : await Parse(path, delimiter));
         }
     }
 }
