519165c8c8bc4b219b61cf57616ef7edf1539403
Sion Pixley <sionpixley@gmail.com>
2020-12-04 15:32:56 -0600

    Initial set up.

---
+++ Sion.CsvParse/Sion.CsvParse/Csv.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sion.CsvParse {
+    public static class Csv {
+        public static async Task<IEnumerable<IEnumerable<string>>> Parse(string path) {
+            IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
+            List<List<string>> data = new List<List<string>>();
+
+            foreach(var line in lines) {
+                data.Add(line.Split(',').ToList());
+            }
+
+            return data;
+        }
+
+        public static async Task<IEnumerable<IEnumerable<string>>> Parse(string path, char delimiter) {
+            IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
+            List<List<string>> data = new List<List<string>>();
+
+            foreach(var line in lines) {
+                data.Add(line.Split(delimiter).ToList());
+            }
+
+            return data;
+        }
+    }
+}
