256e6e992f805b02cbbd3804b8d1df974ed3c937
Sion Pixley <sionpixley@gmail.com>
2021-01-06 19:10:58 -0600

    Add a new method: Replace.

--- Sion.CSVParse/CSV.cs
+++ Sion.CSVParse/CSV.cs
@@ -47,6 +47,56 @@ namespace Sion.CSVParse {
             await System.IO.File.WriteAllLinesAsync(path, newLines, Encoding.UTF8);
         }
 
+        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path) {
+            return (await Parse(path)).Skip(1);
+        }
+
+        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path, char delimiter) {
+            return (await Parse(path, delimiter)).Skip(1);
+        }
+
+        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path, bool hasHeaders) {
+            return ((hasHeaders) ? (await Parse(path)).Skip(1) : await Parse(path));
+        }
+
+        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path, char delimiter, bool hasHeaders) {
+            return ((hasHeaders) ? (await Parse(path, delimiter)).Skip(1) : await Parse(path, delimiter));
+        }
+
+        public static async Task<IEnumerable<string>> GetHeaders(string path) {
+            IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
+            IEnumerable<string> headers = lines.ElementAt(0).Split(',');
+            return headers;
+        }
+
+        public static async Task<IEnumerable<string>> GetHeaders(string path, char delimiter) {
+            IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
+            IEnumerable<string> headers = lines.ElementAt(0).Split(delimiter);
+            return headers;
+        }
+
+        public static IEnumerable<IEnumerable<string>> GetXValues(IEnumerable<IEnumerable<string>> dataset) {
+            List<List<string>> xValues = new List<List<string>>();
+            foreach(var row in dataset) {
+                List<string> current = new List<string>();
+                List<string> rowList = row.ToList();
+                for(int i = 0; i < (rowList.Count - 1); i += 1) {
+                    current.Add(rowList[i]);
+                }
+                xValues.Add(current);
+            }
+            return xValues;
+        }
+
+        public static IEnumerable<string> GetYValues(IEnumerable<IEnumerable<string>> dataset) {
+            List<string> yValues = new List<string>();
+            foreach(var row in dataset) {
+                List<string> rowList = row.ToList();
+                yValues.Add(rowList[^1]);
+            }
+            return yValues;
+        }
+
         public static async Task<IEnumerable<IEnumerable<string>>> Parse(string path) {
             IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
             List<IEnumerable<string>> data = new List<IEnumerable<string>>();
@@ -69,54 +119,46 @@ namespace Sion.CSVParse {
             return data;
         }
 
-        public static async Task<IEnumerable<string>> GetHeaders(string path) {
+        public static async Task Replace(string path, string replace, string newValue) {
             IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
-            IEnumerable<string> headers = lines.ElementAt(0).Split(',');
-            return headers;
-        }
+            List<string> newLines = new List<string>();
 
-        public static async Task<IEnumerable<string>> GetHeaders(string path, char delimiter) {
-            IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
-            IEnumerable<string> headers = lines.ElementAt(0).Split(delimiter);
-            return headers;
-        }
-
-        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path) {
-            return (await Parse(path)).Skip(1);
-        }
-
-        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path, char delimiter) {
-            return (await Parse(path, delimiter)).Skip(1);
-        }
-
-        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path, bool hasHeaders) {
-            return ((hasHeaders) ? (await Parse(path)).Skip(1) : await Parse(path));
-        }
-
-        public static async Task<IEnumerable<IEnumerable<string>>> GetData(string path, char delimiter, bool hasHeaders) {
-            return ((hasHeaders) ? (await Parse(path, delimiter)).Skip(1) : await Parse(path, delimiter));
-        }
-
-        public static IEnumerable<string> GetYValues(IEnumerable<IEnumerable<string>> dataset) {
-            List<string> yValues = new List<string>();
-            foreach(var row in dataset) {
-                List<string> rowList = row.ToList();
-                yValues.Add(rowList[^1]);
-            }
-            return yValues;
-        }
-
-        public static IEnumerable<IEnumerable<string>> GetXValues(IEnumerable<IEnumerable<string>> dataset) {
-            List<List<string>> xValues = new List<List<string>>();
-            foreach(var row in dataset) {
-                List<string> current = new List<string>();
-                List<string> rowList = row.ToList();
-                for(int i = 0; i < (rowList.Count - 1); i += 1) {
-                    current.Add(rowList[i]);
+            foreach(var line in lines) {
+                if(line.Contains(",,")) {
+                    string newLine = line;
+                    while(newLine.Contains(",,")) {
+                        int index = newLine.IndexOf(",,");
+                        newLine = $"{newLine[0..(index + 1)]}{newValue}{newLine[(index + 1)..]}";
+                    }
+                    newLines.Add(newLine);
                 }
-                xValues.Add(current);
+                else {
+                    newLines.Add(line);
+                }
             }
-            return xValues;
+
+            await System.IO.File.WriteAllLinesAsync(path, newLines, Encoding.UTF8);
+        }
+
+        public static async Task Replace(string path, string replace, string newValue, char delimiter) {
+            IEnumerable<string> lines = await System.IO.File.ReadAllLinesAsync(path, Encoding.UTF8);
+            List<string> newLines = new List<string>();
+
+            foreach(var line in lines) {
+                if(line.Contains($"{delimiter}{delimiter}")) {
+                    string newLine = line;
+                    while(newLine.Contains($"{delimiter}{delimiter}")) {
+                        int index = newLine.IndexOf($"{delimiter}{delimiter}");
+                        newLine = $"{newLine[0..(index + 1)]}{newValue}{newLine[(index + 1)..]}";
+                    }
+                    newLines.Add(newLine);
+                }
+                else {
+                    newLines.Add(line);
+                }
+            }
+
+            await System.IO.File.WriteAllLinesAsync(path, newLines, Encoding.UTF8);
         }
     }
 }
